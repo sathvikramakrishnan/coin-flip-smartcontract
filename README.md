8e0817f42d31043f458e70852abd7dfca816b5c9
Sathvik R <sathvikr2004@gmail.com>
2024-07-10 21:04:14 +0530

    contract done

--- README.md
+++ README.md
@@ -17,6 +17,18 @@ This program uses chainlink vrf v2.5 to generate random number that is than mapp
 
 Chainlink Automation with custom logic is used to start the game and transfer the owner's funds into the contract as soon as a player enters.
 
+## A note on private keys
+This contract is not safe to use with legitimate private keys.\
+Use only those private keys with no real assets.
+
+## A note on chainlink vrf2.5 scripts
+This contract uses chainlink vrf2.5.
+
+A modified version of `VRFCoordinatorV2Mock.sol` is created in the src directory under the name `VRFCoordinatorV2PlusMock.sol`\
+They both provide roughly the same functionailties; the newer program has modifications to implement `AddConsumer` function which is not available in the official library.
+
+Shoutout to EngrPips for this code!
+
 ## Documentation
 
 https://book.getfoundry.sh/
--- src/CoinFlip.sol
+++ src/CoinFlip.sol
@@ -24,24 +24,32 @@
 
 pragma solidity ^0.8.18;
 
+import {IVRFCoordinatorV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/interfaces/IVRFCoordinatorV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
 /**
  * @title Coin flip smart contract
  * @author Sathvik
  * @notice A coin flipping smart contract game
- * @dev Implements Chainlink vrf2.5
+ * @notice The user wins of the coin lands on head; the owner wins if it lands on tail.
+ * @dev Implements Chainlink vrf2.5 with a custom made mock vrf script
  */
-contract CoinFlip {
-    error CoinFlip__NotEnoughEthSent();
+contract CoinFlip is VRFConsumerBaseV2Plus {
+    error CoinFlip__NotEnoughEthSent(uint256 value);
     error CoinFlip__GameNotOpen();
     error CoinFlip__UpkeepNotNeeded(
         uint256 currentBalance,
         uint256 contractState
     );
+    error CoinFlip__TransactionFailed(address winner);
+    error CoinFlip__NotTheOwner(address sender);
 
     /**Type declarations */
     enum CoinState {
-        HEAD, // 0
-        TAIL // 1
+        TAIL, // 0
+        HEAD, // 1
+        TOSS // 2
     }
 
     enum ContractState {
@@ -50,34 +58,76 @@ contract CoinFlip {
     }
 
     /**State variables */
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    address private i_owner;
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
+    IVRFCoordinatorV2Plus private immutable i_vrfCoordinator;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
 
     uint256 private s_lastTimeStamp;
     address private s_recentWinner;
-    address payable private s_player;
+    address private s_player;
     CoinState private s_coinState;
     ContractState private s_contractState;
+    bool private s_ownerHasEntered;
 
     event EnteredGame(address indexed player);
+    event OwnerEnteredGame(address indexed owner);
+    event RequestedWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner, CoinState indexed flipOutput);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 keyHash,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
+        i_owner = msg.sender;
+
         i_entranceFee = entranceFee;
         i_interval = interval;
+        i_vrfCoordinator = IVRFCoordinatorV2Plus(vrfCoordinator);
+        i_keyHash = keyHash;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
 
         s_lastTimeStamp = block.timestamp;
         s_contractState = ContractState.OPEN;
+        s_ownerHasEntered = false;
     }
 
     function enterCoinFlipGame() external payable {
         if (msg.value < i_entranceFee) {
-            revert CoinFlip__NotEnoughEthSent();
+            revert CoinFlip__NotEnoughEthSent(msg.value);
         }
         if (s_contractState == ContractState.CALCULATING) {
             revert CoinFlip__GameNotOpen();
         }
-        s_player = (payable(msg.sender));
+
+        s_player = (msg.sender);
         emit EnteredGame(msg.sender);
+    }
+
+    function OwnerEntersCoinFlipGame() external payable {
+        if (msg.sender != i_owner) {
+            revert CoinFlip__NotTheOwner(msg.sender);
+        }
+        if (msg.value < i_entranceFee) {
+            revert CoinFlip__NotEnoughEthSent(msg.value);
+        }
+        if (s_contractState == ContractState.CALCULATING) {
+            revert CoinFlip__GameNotOpen();
+        }
+
+        s_ownerHasEntered = true;
+        emit OwnerEnteredGame(i_owner);
     }
 
     /**
@@ -86,20 +136,25 @@ contract CoinFlip {
      * 1. time interval has passed between 2 contract runs
      * 2. contract has non-zero balance
      * 3. checks if there is a player in the game
+     * 4. checks if the owner is in the game
      */
     function checkUpkeep(
         bytes memory /** checkData*/
     ) public view returns (bool upkeepNeeded, bytes memory) {
         bool timeHasPassed = (block.timestamp - s_lastTimeStamp > i_interval);
-        bool isOpen = ContractState.OPEN == s_contractState;
+        bool isOpen = (ContractState.OPEN == s_contractState);
         bool hasFunds = address(this).balance > 0;
         bool hasPlayer = s_player != address(0);
 
-        upkeepNeeded = (hasPlayer && hasFunds && isOpen && timeHasPassed);
+        upkeepNeeded = (hasPlayer &&
+            hasFunds &&
+            isOpen &&
+            timeHasPassed &&
+            s_ownerHasEntered);
         return (upkeepNeeded, "0x0");
     }
 
-    function performUpkeep(bytes calldata /**performData*/) external {
+    function performUpkeep(bytes calldata /** performData */) external {
         (bool upkeepNeeded, ) = checkUpkeep("");
         if (!upkeepNeeded) {
             revert CoinFlip__UpkeepNotNeeded(
@@ -109,6 +164,53 @@ contract CoinFlip {
         }
 
         s_contractState = ContractState.CALCULATING;
+        s_coinState = CoinState.TOSS;
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash, // Updated for VRF v2.5
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+
+        emit RequestedWinner(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256 /** requestId */,
+        uint256[] calldata randomWords
+    ) internal override {
+        address winner;
+        uint256 coinState = randomWords[0] % 2;
+        if (coinState == 1) {
+            winner = s_player;
+            s_coinState = CoinState.HEAD;
+        } else {
+            winner = i_owner;
+            s_coinState = CoinState.TAIL;
+        }
+
+        // Clean up stuff
+        s_recentWinner = winner;
+        s_contractState = ContractState.OPEN;
+        s_player = address(0);
+        s_lastTimeStamp = block.timestamp;
+        s_ownerHasEntered = false;
+
+        emit WinnerPicked(winner, s_coinState);
+
+        (bool success, ) = payable(winner).call{value: address(this).balance}(
+            ""
+        );
+        if (!success) {
+            revert CoinFlip__TransactionFailed(winner);
+        }
     }
 
     /**
@@ -124,5 +226,9 @@ contract CoinFlip {
 
     function getRecentWinner() external view returns (address) {
         return s_recentWinner;
+    }
+
+    function getCoinState() external view returns (CoinState) {
+        return s_coinState;
     }
 }
